d28825103ff80f613bca4376913b51d1e30ff3ec
StealthyPanda <shaikm259@gmail.com>
2022-09-13 00:52:33 +0530

    Implemented a single intersection simulation
    The class `Intersection` handles all the lights, lanes and cars at a given intersection
    
    All that is left is implementation of the algos that are gonna control the traffic lights.

--- libs.py
+++ libs.py
@@ -1,11 +1,17 @@
+from pickle import STOP
 from random import randint
-
+from typing import Tuple
 
 STOPPED = 0
 MOVING = 1
 
 GREEN = 1
 RED = 0
+
+
+def mod(x) : return x if x >= 0 else -x
+def sign(x) : return 1 if x > 0 else (-1 if x < 0 else 0)
+
 
 class RoadMap:
 
@@ -25,10 +31,10 @@ class RoadMap:
                 i += 1
                 self.intersections.append({
                     'position' : [rlength * each, rlength * i],
-                    'up' : GREEN,
-                    'down' : GREEN,
-                    'left' : GREEN,
-                    'right' : GREEN,
+                    'up' : RED,
+                    'down' : RED,
+                    'left' : RED,
+                    'right' : RED,
                 })
                 self.ips.append([rlength * each, rlength * i])
         for x in self.intersections:
@@ -53,3 +59,178 @@ class RoadMap:
         for car in self.cars:
             if car['position'] in self.ips:
                 car['state'] = MOVING
+
+
+
+class Intersection:
+
+    def __init__(self, center : Tuple[int, int] = None, rlength : int = 60) -> None:
+        
+        self.lightstate = {
+            'up' : RED,
+            'down' : RED,
+            'left' : RED,
+            'right' : RED,
+        }
+
+        self.carstate = {
+            'up' : [],
+            'down' : [],
+            'right' : [],
+            'left' : []
+        }
+
+        self.mapping = {
+            (1, 0) : 'left',
+            (-1, 0) : 'right',
+            (0, 1) : 'up',
+            (0, -1) : 'down',
+        }
+
+        if center is None: center = (rlength, rlength)
+
+        self.rlength = rlength
+        self.center = center
+        self.cars = []
+        self.t = 0
+    
+    def categorisecar(self, car):
+
+        [x, y] = car['position']
+        
+        if x > self.center[0]: self.carstate['right'].append(car)
+        elif x < self.center[0]: self.carstate['left'].append(car)
+        elif y < self.center[1]: self.carstate['up'].append(car)
+        elif y > self.center[1]: self.carstate['down'].append(car)
+        else: print("Something's not right I can feel it")
+    
+    def addcar(self, position : list[int, int], velocity : int,) -> None:
+        
+        [x, y] = position
+
+        if mod(x - self.center[0]) > self.rlength: raise TypeError(f"Invalid x coordinate of car : {x}")
+        if mod(y - self.center[1]) > self.rlength: raise TypeError(f"Invalid y coordinate of car : {y}")
+
+        if x != self.center[0] and y != self.center[1]:
+            raise TypeError(f"Car not on a valid road! Either x or y coordinate must be along center of intersection : {(x, y)}")
+
+        direction = (sign(self.center[0] - x), sign(self.center[1] - y))
+
+        car = {
+            'position' : [x, y],
+            'velocity' : velocity,
+            'state' : MOVING,
+            'direction' : direction
+        }
+
+        self.cars.append(car)
+        self.categorisecar(car)
+    
+    def movecars(self) -> None:
+        for car in self.cars:
+            if car['state'] == MOVING:
+                dir = car['direction']
+
+                xmov, ymov = car['velocity'] * car['direction'][0], car['velocity'] * car['direction'][1]
+
+                [x, y] = car['position']
+
+                if x == self.center[0]:
+                    if (mod(ymov) >= mod(y - self.center[1])) and (self.lightstate[self.mapping[dir]] == RED):
+                        ymov = self.center[1] - y - car['direction'][1]
+                        car['state'] = STOPPED
+                else:
+                    if (mod(xmov) >= mod(x - self.center[0])) and (self.lightstate[self.mapping[dir]] == RED):
+                        xmov = self.center[0] - x - car['direction'][0]
+                        car['state'] = STOPPED
+                
+                car['position'][0] += xmov
+                car['position'][1] += ymov
+
+    
+    
+    def printcars(self):
+        print(f"At t = {self.t}")
+        for each in self.carstate.keys():
+            print(each,':')
+            for x in (self.carstate[each]):
+                print(x)
+    
+    def updateframe(self) -> None:
+        self.t += 1
+        self.movecars()
+
+    def __repr__(self) -> str:
+        string = ""
+
+        string += f"Intersection at : {self.center}\n"
+
+        string += f"At t = {self.t},\n"
+        for x in self.lightstate.keys():
+            string += f"{x} : {['RED', 'GREEN'][self.lightstate[x]]},  "
+        string += "\nCars:\n"
+        for x in self.carstate.keys():
+            string += f"{x} : {len(self.carstate[x])},  "
+
+
+        lb, lf, rb, rf, ub, uf, db, df = 0, 0, 0, 0, 0, 0, 0, 0
+
+
+        for car in self.carstate['up']:
+            if car['position'][1] == self.center[1] - 1: uf += 1
+            elif car['position'][1] < self.center[1] : ub += 1
+        
+        for car in self.carstate['down']:
+            if car['position'][1] == self.center[1] + 1: df += 1
+            elif car['position'][1] > self.center[1] : db += 1
+        
+        for car in self.carstate['left']:
+            if car['position'][0] == self.center[0] - 1: lf += 1
+            elif car['position'][0] < self.center[0] : lb += 1
+        
+        for car in self.carstate['right']:
+            if car['position'][0] == self.center[0] + 1: rf += 1
+            elif car['position'][0] > self.center[0]: rb += 1
+        
+        syms = [lb, lf, rb, rf, ub, uf, db, df]
+        for each in range(len(syms)):
+            if syms[each] < 10: syms[each] = '0' + str(syms[each])
+            syms[each] = str(syms[each])
+        
+        [lb, lf, rb, rf, ub, uf, db, df] = syms
+        
+        lights = [self.lightstate['left'], self.lightstate['right'], self.lightstate['up'], self.lightstate['down']]
+
+        lights = list(map(lambda x: 'G' if x == GREEN else 'R', lights))
+
+        [ll, rl, ul, dl] = lights
+
+        string += f"""
+
+At t = {self.t}
+                      |          |
+                      |          |
+                      |  ┌────┐  |
+                      |  | {ub} |  |  
+                ┌───┐ |  └────┘  |┌──O──┐
+                O {ll} | |  ┌────┐  ||  {ul}  |
+                └───┘ |  | {uf} |  |└─────┘
+──────────────────────┘  └────┘  └──────────────────────
+
+           ┌────┐┌────┐         ┌────┐┌────┐
+           | {lb} || {lf} |         | {rf} || {rb} |
+           └────┘└────┘         └────┘└────┘
+
+──────────────────────┐  ┌────┐  ┌──────────────────────
+               ┌─────┐|  | {df} |  |┌───┐
+               |  {dl}  ||  └────┘  || {rl} O
+               └──O──┘|  ┌────┐  |└───┘
+                      |  | {db} |  |
+                      |  └────┘  |
+                      |          |
+                      |          |
+
+"""
+
+
+        return string
--- runner.py
+++ runner.py
@@ -1,5 +1,42 @@
 from libs import *
 
-rm = RoadMap(3, 2)
+inter = Intersection()
 
-rm.spawncars()
+for x in [5, 10, 22]:
+    inter.addcar(
+        position = [x, 60],
+        velocity = randint(1, 4)
+    )
+
+
+for x in [7, 11, 25]:
+    inter.addcar(
+        position = [60, x],
+        velocity = randint(1, 4)
+    )
+
+for x in [7, 11, 25]:
+    inter.addcar(
+        position = [60, 60 + x],
+        velocity = randint(1, 4)
+    )
+
+for x in [7, 11, 25]:
+    inter.addcar(
+        position = [60 + x, 60],
+        velocity = randint(1, 4)
+    )
+
+inter.lightstate['left'] = GREEN
+
+print(inter)
+
+for i in range(19) : inter.updateframe()
+
+print(inter)
+print()
+
+inter.updateframe()
+
+print(inter)
+inter.printcars()
